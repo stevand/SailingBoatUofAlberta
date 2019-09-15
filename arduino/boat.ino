2a884c8cfad7e0c7a86e21bdfddbf436e2f2ec18
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2019-09-15 12:56:44 -0600

    added placeholders for all inputs and removed requirement for leading zeros when passing angles less than 3 digits

--- arduino/boat.ino
+++ arduino/boat.ino
@@ -2,7 +2,7 @@
 
 //take care to properly connect components
 const int RUDDER_PIN = 3;
-const int SAIL_PIN = 4; 
+const int SAIL_PIN = 4;
 
 Servo rudder;
 Servo sail;
@@ -25,56 +25,98 @@ void loop()
 }
 
 //blocking function
-String readInput(){
-	while(!Serial.available())
+String readInput()
+{
+	while (!Serial.available())
 		;
 	return Serial.readString();
 }
 
 //parses int in the range [start, stop)
-int parseInt(String str, int start, int stop){
+int parseInt(String str, int start, int stop)
+{
 	int total = 0;
-	for (int i = start; i < stop; i++){
+	for (int i = start; i < stop; i++)
+	{
 		total *= 10;
 		total += str.charAt(i) - '0'; //converts ascii digit to int
 	}
 	return total;
 }
 
-void setRudder(int angle){
+void setRudder(int angle)
+{
 	Serial.print("Setting angle of rudder to ");
 	Serial.print(angle);
 	Serial.print(" degrees.\n");
 	rudder.write(angle);
 }
 
-void setSail(int angle){
+void setSail(int angle)
+{
 	Serial.print("Setting angle of sail to ");
 	Serial.print(angle);
 	Serial.print(" degrees.\n");
 	sail.write(angle);
 }
 
+void sendHeading()
+{
+	Serial.println(0.0);
+}
+
+void sendPosition()
+{
+	Serial.print(0.0);
+	Serial.print(',');
+	Serial.println(0.0);
+}
+
+void sendWindspeed()
+{
+	Serial.println(0.0);
+}
+
+void sendWindDirection()
+{
+	Serial.println(0.0);
+}
+
 /*
 first char determines the type of request
-requsts must match formats below exactly (leading zeros on numbers if necessary)
 
 rxxx: set rudder angle to xxx degrees
 sxxx: set sail angle to xxx degrees
+h: return heading
+p: return position
+w: return windspeed
+d: return wind direction
 */
-void handleInput(String input){
-	switch (input.charAt(0))
+void handleInput(String input)
+{
 	int angle;
+	switch (input.charAt(0))
 	{
+	case 'h':
+		sendHeading();
+		break;
+	case 'p':
+		sendPosition();
+		break;
+	case 'w':
+		sendWindspeed();
+		break;
+	case 'd':
+		sendWindDirection();
+		break;
 	case 'r':
-		angle = parseInt(input, 1, 4); 
+		angle = parseInt(input, 1, input.length());
 		setRudder(angle);
 		break;
 	case 's':
-		angle = parseInt(input, 1, 4);
+		angle = parseInt(input, 1, input.length());
 		setSail(angle);
 		break;
-	
 	default:
 		Serial.print("Invalid Command");
 	}
