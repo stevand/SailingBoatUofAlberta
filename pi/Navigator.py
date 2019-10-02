49c190288baf9db6da8066d55dd7d5ffecd99822
wagonhelm <jkf@ualberta.ca>
2019-10-02 12:41:43 -0600

    merged and renamed setters/getters

--- pi/Navigator.py
+++ pi/Navigator.py
@@ -1,46 +1,46 @@
-
+import boat_driver as bd
 
 class Navigator:
-    def __init__(self, abstract_driver):
-        self._driver = abstract_driver
+    def __init__(self):
+        self._driver = bd.AbstractBoatDriver
         # Current side of the boat the wind is coming off of
         self.tack = None
         # Sail tolerance is how close to the wind the boat is able to sail
         self.tolerance = 30
-        # Error is the maximum allowed amount of heading error
+        # Error is the maximum allowed amount of get_heading error
         self.error = 1
         # Initialize winch to 0 Degrees
-        self._driver.sail(0)
+        self._driver.set_sail(0)
 
     def adjust(self):
-        """Adjusts the angle of the sails to maximize velocity on current heading"""
-        self._driver.sail(min(abs(self._get_wind_rel(heading=self._driver.heading())), 90))
+        """Adjusts the angle of the sails to maximize velocity on current get_heading"""
+        self._driver.set_sail(min(abs(self._get_wind_rel(heading=self._driver.get_heading())), 90))
 
     def turn(self, new_heading):
         """Turns the boat to face the new_heading. Returns -1 if not possible, 1 otherwise"""
-        # Get distance between new heading and wind direction
+        # Get distance between new get_heading and wind direction
         if abs(self._get_wind_rel(heading=new_heading)) < self.tolerance:
             return -1
 
         # If you need to turn to port
-        if new_heading > (self._driver.heading() + 180):
-            while abs(self._driver.heading() - new_heading) > self.error:
+        if new_heading > (self._driver.get_heading() + 180):
+            while abs(self._driver.get_heading() - new_heading) > self.error:
                 # Turn port
                 self.adjust()
                 pass
         # If you need to turn to starboard
         else:
-            while abs(self._driver.heading() - new_heading) > self.error:
+            while abs(self._driver.get_heading() - new_heading) > self.error:
                 # Turn starboard
                 self.adjust()
                 pass
 
     def turn_rel(self, degrees):
-        """Turns the boat relative to its current heading
+        """Turns the boat relative to its current get_heading
         Starboard turn ranges from (0 -> 180]
         Port turn ranges from (0 -> -180]
         """
-        new_heading = (self._driver.heading() + degrees) % 360
+        new_heading = (self._driver.get_heading() + degrees) % 360
         self.turn(new_heading)
         return None
 
@@ -51,19 +51,19 @@ class Navigator:
         A positive number from (0 ->  180] implies your on a starboard tack
         """
         # If wind direction is greater then boat direction
-        if self._driver.wind_direction() > heading:
+        if self._driver.get_wind_direction() > heading:
             # Get shortest wind angle relative to boat
-            if self._driver.wind_direction() > (heading + 180):
-                wind_rel = -(360 - self._driver.wind_direction() + heading)
+            if self._driver.get_wind_direction() > (heading + 180):
+                wind_rel = -(360 - self._driver.get_wind_direction() + heading)
             else:
-                wind_rel = self._driver.wind_direction() - heading
+                wind_rel = self._driver.get_wind_direction() - heading
         # If boat direction is greater then wind direction
         else:
             # Get shortest wind angle relative to the boat
-            if heading > (self._driver.wind_direction() + 180):
-                wind_rel = (360 - heading + self._driver.wind_direction())
+            if heading > (self._driver.get_wind_direction() + 180):
+                wind_rel = (360 - heading + self._driver.get_wind_direction())
             else:
-                wind_rel = -(heading - self._driver.wind_direction())
+                wind_rel = -(heading - self._driver.get_wind_direction())
         if wind_rel > 0:
             self.tack = "STARBOARD"
         else:
