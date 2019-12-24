36f3bbed6cf612ecf04c0309319502674e40eda5
yash-bhandari <yashaswi@ualberta.ca>
2019-12-24 15:58:01 -0700

    reformatted

--- arduino/boat/XBus.cpp
+++ arduino/boat/XBus.cpp
@@ -2,127 +2,148 @@
 #include <Wire.h>
 #define M_PI 3.14159265358979323846
 
-XBus::XBus(uint8_t address) : address(address){
-	for(int i = 0; i < 4; ++i){
-		if(i < 3){
-			accel[i] =0;
-			rot[i] =0;
-			mag[i] =0;
+XBus::XBus(uint8_t address) : address(address)
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		if (i < 3)
+		{
+			accel[i] = 0;
+			rot[i] = 0;
+			mag[i] = 0;
 		}
 		quat[i] = 0;
 	}
 }
 
-void XBus::dataswapendian(uint8_t* data, uint8_t length){
+void XBus::dataswapendian(uint8_t *data, uint8_t length)
+{
 	uint8_t cpy[length];
-	memcpy(cpy,data,length);
-	for(int i = 0; i < length/4; i++){
-		for(int j = 0; j < 4; j++){
-			data[j+i*4] = cpy[3-j+i*4];
+	memcpy(cpy, data, length);
+	for (int i = 0; i < length / 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			data[j + i * 4] = cpy[3 - j + i * 4];
 		}
 	}
 }
 
-void XBus::parseData(uint8_t* data, uint8_t datalength){
-	if(datalength < 2)
-	return;
-	
+void XBus::parseData(uint8_t *data, uint8_t datalength)
+{
+	if (datalength < 2)
+		return;
+
 	uint8_t actualMID = data[0];
-	if(actualMID == 0x10 || actualMID == 0x20 || actualMID == 0x40 || actualMID == 0x80 || actualMID == 0xC0 || actualMID == 0xE0){
+	if (actualMID == 0x10 || actualMID == 0x20 || actualMID == 0x40 || actualMID == 0x80 || actualMID == 0xC0 || actualMID == 0xE0)
+	{
 		uint8_t length = data[2];
-		switch(((uint16_t)data[1] |((uint16_t)data[0]<<8)) & (uint16_t)0xFFF0){
+		switch (((uint16_t)data[1] | ((uint16_t)data[0] << 8)) & (uint16_t)0xFFF0)
+		{
 		case (uint16_t)DataID::QUAT:
-			dataswapendian(data+3, sizeof(float)*4);
-			memcpy(quat, data+3, sizeof(float)*4);
+			dataswapendian(data + 3, sizeof(float) * 4);
+			memcpy(quat, data + 3, sizeof(float) * 4);
 			break;
 		case (uint16_t)DataID::ACCEL:
-			dataswapendian(data+3, 3*sizeof(float));
-			memcpy(accel, data+3, sizeof(float)*3);
+			dataswapendian(data + 3, 3 * sizeof(float));
+			memcpy(accel, data + 3, sizeof(float) * 3);
 			break;
 		case (uint16_t)DataID::MAG:
-			dataswapendian(data+3, 3*sizeof(float));
-			memcpy(mag, data+3, sizeof(float)*3);
+			dataswapendian(data + 3, 3 * sizeof(float));
+			memcpy(mag, data + 3, sizeof(float) * 3);
 			break;
 		case (uint16_t)DataID::ROT:
-			dataswapendian(data+3, 3*sizeof(float));
-			memcpy(rot, data+3, sizeof(float)*3);
+			dataswapendian(data + 3, 3 * sizeof(float));
+			memcpy(rot, data + 3, sizeof(float) * 3);
 			break;
 		}
-		parseData(data+3+length, datalength - length - 3);
-	}else{
+		parseData(data + 3 + length, datalength - length - 3);
+	}
+	else
+	{
 		uint8_t length = data[1];
-		if(actualMID == (uint8_t)MesID::DATA || actualMID == (uint8_t)MesID::DATA2)
-		parseData(data+2, length);
+		if (actualMID == (uint8_t)MesID::DATA || actualMID == (uint8_t)MesID::DATA2)
+			parseData(data + 2, length);
 	}
 }
 
-void XBus::readPipeStatus(){
+void XBus::readPipeStatus()
+{
 	Wire.beginTransmission(address);
 	Wire.write(XSENS_PIPE_STATUS);
 	Wire.endTransmission();
-	
 
-	Wire.requestFrom(address,(uint8_t)4);
-	if(Wire.available()>0) {
-		for(int i = 0; i < 4; i++){
+	Wire.requestFrom(address, (uint8_t)4);
+	if (Wire.available() > 0)
+	{
+		for (int i = 0; i < 4; i++)
+		{
 			data[i] = Wire.read();
 		}
 	}
-	notificationSize = (uint16_t)data[0] | ((uint16_t)data[1]<<8);
-	measurementSize = (uint16_t)data[2] | ((uint16_t)data[3]<<8);
+	notificationSize = (uint16_t)data[0] | ((uint16_t)data[1] << 8);
+	measurementSize = (uint16_t)data[2] | ((uint16_t)data[3] << 8);
 }
-void XBus::readPipeNotif(){
+void XBus::readPipeNotif()
+{
 	Wire.beginTransmission(address);
 	Wire.write(XSENS_NOTIF_PIPE);
 	Wire.endTransmission();
-	
 
-	Wire.requestFrom(address,notificationSize);
-	if(Wire.available()>0) {
-		for(int i = 0; i < notificationSize; ++i){
+	Wire.requestFrom(address, notificationSize);
+	if (Wire.available() > 0)
+	{
+		for (int i = 0; i < notificationSize; ++i)
+		{
 			datanotif[i] = Wire.read();
 		}
 	}
 }
-void XBus::readPipeMeas(){
+void XBus::readPipeMeas()
+{
 	Wire.beginTransmission(address);
 	Wire.write(XSENS_MEAS_PIPE);
 	Wire.endTransmission();
-	
-	Wire.requestFrom(address,measurementSize);
-	if(Wire.available()>0) {
-		for(int i = 0; i < measurementSize; ++i){
+
+	Wire.requestFrom(address, measurementSize);
+	if (Wire.available() > 0)
+	{
+		for (int i = 0; i < measurementSize; ++i)
+		{
 			datameas[i] = Wire.read();
 		}
 	}
 }
 
-uint8_t* XBus::buildMessage(MesID MID, uint8_t* data, uint8_t length){
-	uint8_t* res = new uint8_t[length+4];
+uint8_t *XBus::buildMessage(MesID MID, uint8_t *data, uint8_t length)
+{
+	uint8_t *res = new uint8_t[length + 4];
 	res[0] = (uint8_t)MID;
 	res[1] = length;
-	if(length > 0 && data != NULL)
-		memcpy(res+2, data, length);
+	if (length > 0 && data != NULL)
+		memcpy(res + 2, data, length);
 	else
 		res[2] = 0;
 	uint8_t checksum = 0xFF;
-	for(int i = 0; i< length+2; i++)
+	for (int i = 0; i < length + 2; i++)
 		checksum -= res[0];
-	res[length+3] = checksum;
-	
+	res[length + 3] = checksum;
+
 	return res;
 }
 
-void XBus::quatToHeading(){
-  // https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
-  pitch = -atan2(2*(quat[0]*quat[1]+quat[2]*quat[3]), 1-2*(quat[1]*quat[1]+quat[2]*quat[2]))/M_PI*180.0;
-  yaw = -atan2(2*(quat[0]*quat[3]+quat[1]*quat[2]), 1-2*(quat[2]*quat[2]+quat[3]*quat[3]))/M_PI*180.0;
-    if (yaw<0)
-      yaw += 360;
-  roll = -asin(2*(quat[0]*quat[2] - quat[3]*quat[1]))/M_PI*180.0;
+void XBus::quatToHeading()
+{
+	// https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+	pitch = -atan2(2 * (quat[0] * quat[1] + quat[2] * quat[3]), 1 - 2 * (quat[1] * quat[1] + quat[2] * quat[2])) / M_PI * 180.0;
+	yaw = -atan2(2 * (quat[0] * quat[3] + quat[1] * quat[2]), 1 - 2 * (quat[2] * quat[2] + quat[3] * quat[3])) / M_PI * 180.0;
+	if (yaw < 0)
+		yaw += 360;
+	roll = -asin(2 * (quat[0] * quat[2] - quat[3] * quat[1])) / M_PI * 180.0;
 }
 
-void XBus::read(){
+void XBus::read()
+{
 	readPipeStatus();
 	readPipeNotif();
 	readPipeMeas();
--- arduino/boat/XSens.h
+++ arduino/boat/XSens.h
@@ -3,27 +3,28 @@
 
 #include "XBus.h"
 
-class XSens{
-	public:
-		XSens(uint8_t address = 0x6b) : address(address), xbus(address), wokeUp(false){}
-		
-		void begin();
-		void updateMeasures();
-		
-		float get_yaw(){return xbus.yaw;}
-    float get_roll(){return xbus.roll;}
-    float get_pitch(){return xbus.pitch;}
-		
-		float* getQuat(){return xbus.quat;}
-		float* getAccel(){return xbus.accel;}
-		float* getMag(){return xbus.mag;}
-		float* getRot(){return xbus.rot;}
-		
-	private:
-		uint8_t address;
-		
-		bool wokeUp;
-		XBus xbus;
+class XSens
+{
+public:
+	XSens(uint8_t address = 0x6b) : address(address), xbus(address), wokeUp(false) {}
+
+	void begin();
+	void updateMeasures();
+
+	float get_yaw() { return xbus.yaw; }
+	float get_roll() { return xbus.roll; }
+	float get_pitch() { return xbus.pitch; }
+
+	float *getQuat() { return xbus.quat; }
+	float *getAccel() { return xbus.accel; }
+	float *getMag() { return xbus.mag; }
+	float *getRot() { return xbus.rot; }
+
+private:
+	uint8_t address;
+
+	bool wokeUp;
+	XBus xbus;
 };
 
 #endif
