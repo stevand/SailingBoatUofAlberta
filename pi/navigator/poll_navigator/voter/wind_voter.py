5af0499835ac8a941265f75f6f7aa927b4310a19
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2020-02-06 22:05:13 -0700

    changed windvoter to use navutils

--- pi/navigator/poll_navigator/voter/wind_voter.py
+++ pi/navigator/poll_navigator/voter/wind_voter.py
@@ -1,11 +1,13 @@
 from . import Voter
+from pi.navutils import smallest_angle_between
 
 class WindVoter(Voter):
 
     def vote(self, heading):
         # finds the smallest angle between the candidate heading and the true wind direction
         t = self._driver.get_wind_dir_true()
-        smallest_angle = min((heading-t)%180, (t-heading)%180)
+        print("true wind is: ", t)
+        smallest_angle = smallest_angle_between(heading, t)
 
         if smallest_angle < 45:
             return 0
