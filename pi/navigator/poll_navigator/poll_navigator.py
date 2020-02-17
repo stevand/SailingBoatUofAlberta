84a0ebcb3ab0a1b3661f25062d8fc9183bdfc757
yash-bhandari <yashaswi@ualberta.ca>
2020-02-17 12:22:11 -0700

    added channel/maneuver voters to poll_nav, can tack upwind

--- pi/navigator/poll_navigator/poll_navigator.py
+++ pi/navigator/poll_navigator/poll_navigator.py
@@ -1,6 +1,6 @@
 from threading import Thread
 from .. import AbstractNavigator
-from .voter import WaypointVoter, WindVoter
+from .voter import WaypointVoter, WindVoter, ChannelVoter, ManueverVoter
 from pi.navutils import dist
 from time import sleep
 
@@ -11,15 +11,18 @@ def navigate(interval, nav, driver, helmsman):
             next_waypoint = nav.get_waypoint()
             boat = driver.get_position()
             while next_waypoint and dist(boat, next_waypoint) < nav.waypoint_dist:
-                print("next")
                 nav.del_waypoint()
+                nav._prev = next_waypoint
                 next_waypoint = nav.get_waypoint()
+                print("going to:", next_waypoint)
 
             if next_waypoint:
                 helmsman.maximize_speed = True
                 goto = nav.poll()
                 helmsman.turn(goto)
+                nav.curr_heading = goto
                 print("Chosen heading:", goto)
+                print(*nav.headings[goto])
                 # print(goto, helmsman.desired_heading)
             else:
                 print('minimizing speed')
@@ -31,9 +34,13 @@ class PollNavigator(AbstractNavigator):
     def __init__(self, driver, helmsman, interval=0.3, num_headings=180, **kwargs):
         super().__init__(driver, helmsman, **kwargs)
         # constructs all necessary voters and their weights
+        self._prev = driver.get_position()
+        self.curr_heading = -1 #initialized to impossible value
         self._voters = [
             WaypointVoter(driver, self.get_waypoint),
-            WindVoter(driver)
+            WindVoter(driver),
+            ChannelVoter(driver, lambda: (self._prev, self.get_waypoint())),
+            ManueverVoter(driver, lambda: self.curr_heading)
         ]
         # constructs a dictionary that holds approval for each candidate heading
         self.headings = {2*i: 0 for i in range(num_headings)}
@@ -46,8 +53,9 @@ class PollNavigator(AbstractNavigator):
         """Polls all voters and returns the heading with the highest approval"""
         best_heading = -1
         for heading in self.headings.keys():
-            self.headings[heading] = sum(voter.vote(heading)
-                                         for voter in self._voters)
-            if best_heading == -1 or self.headings[best_heading] < self.headings[heading]:
+            self.headings[heading] = [voter.vote(heading)
+                                         for voter in self._voters]
+            self.headings[heading].insert(0, sum(self.headings[heading]))
+            if best_heading == -1 or self.headings[best_heading][0] < self.headings[heading][0]:
                 best_heading = heading
         return best_heading
