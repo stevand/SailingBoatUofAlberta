9ed709645e0cc02f43f1f90803da2df921492778
yash-bhandari <yashaswi@ualberta.ca>
2020-02-17 10:45:44 -0700

    removed debugging print statement

--- pi/navigator/poll_navigator/voter/wind_voter.py
+++ pi/navigator/poll_navigator/voter/wind_voter.py
@@ -6,7 +6,6 @@ class WindVoter(Voter):
     def vote(self, heading):
         # finds the smallest angle between the candidate heading and the true wind direction
         t = self._driver.get_wind_dir_true()
-        print("true wind is: ", t)
         smallest_angle = smallest_angle_between(heading, t)
 
         if smallest_angle < 45:
