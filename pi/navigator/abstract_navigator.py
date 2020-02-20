7bbabab32a26bbe95a14ec2266c456b6af4a79ab
yash-bhandari <yashaswi@ualberta.ca>
2020-02-20 02:05:56 -0700

    rewrote abstract_navigator to use interval_repeater

--- pi/navigator/abstract_navigator.py
+++ pi/navigator/abstract_navigator.py
@@ -1,23 +1,55 @@
 import abc
+from pi.boat_driver.abstract_boat_driver import AbstractBoatDriver
+from pi.helmsman import Helmsman
+from threading import Thread
+from pi.navutils import dist
+from time import sleep
+from pi.interval_repeater import IntervalRepeater
+from typing import List, Tuple
 
-class AbstractNavigator(abc.ABC):
+Waypoint = Tuple[int, int]
+
+class AbstractNavigator(IntervalRepeater, abc.ABC):
     """
     Abstraction layer built on top of a BoatDriver and (optionally) a Helmsman.
     Should be able to navigate to waypoints that are passed to it.
     """
 
-    @abc.abstractmethod
-    def __init__(self, driver, helmsman, enabled=True, waypoint_dist=0.2, **kwargs):
-        """
-        Initializes the navigator with the given driver and helmsman.
-        """
+    def __init__(self, driver: AbstractBoatDriver, helmsman: Helmsman, enabled: bool = True, interval: float = 0.5, waypoint_dist: float = 0.2, **kwargs):
+        super().__init__(interval=interval, enabled=enabled)
         self._driver = driver
         self._helmsman = helmsman
-        self._waypoints = []
-        self._enabled = enabled
+        self.__waypoints = []
+        self.__last_waypoint = self._driver.get_position()
         self.waypoint_dist = waypoint_dist
+        self._start_interval_repeater()
 
-    def add_waypoint(self, waypoint, i=-1) -> int:
+    def _interval_process(self):
+        """
+        Repeatedly exectuted by IntervalRepeater mixin while enabled
+        """
+        self._clear_reached_waypoints()
+        self._navigate_to_curr_waypoint()
+
+    def _clear_reached_waypoints(self):
+        """
+        Clears all waypoints that are within waypoint_dist of the boat
+        """
+        next_waypoint = self.get_waypoint()
+        boat_pos = self._driver.get_position()
+        while (next_waypoint and dist(boat_pos, next_waypoint) < self.waypoint_dist):
+            self.del_waypoint()
+            next_waypoint = self.get_waypoint()
+
+    @abc.abstractmethod
+    def _navigate_to_curr_waypoint(self):
+        """
+        This method should contain the navigator's main logic and will be executed at regular intervals while the navigator is enabled.
+        The current waypoint can be accessed with self.get_waypoint()
+        """
+        pass
+
+    def add_waypoint(self, waypoint: Waypoint, i: int = -1) -> int:
         """
         Adds the given waypoint to the current waypoint queue (at the end, by default).
         Args:
@@ -27,12 +59,12 @@ class AbstractNavigator(abc.ABC):
             The new length of the waypoint queue
         """
         if i == -1:
-            self._waypoints.append(waypoint)
+            self.__waypoints.append(waypoint)
         else:
-            self._waypoints.insert(i, waypoint)
-        return len(self._waypoints)
+            self.__waypoints.insert(i, waypoint)
+        return len(self.__waypoints)
 
-    def del_waypoint(self, i=0) -> int:
+    def del_waypoint(self, i: int = 0) -> int:
         """
         Deletes the i-th waypoint in the waypoint queue
         Args:
@@ -40,28 +72,27 @@ class AbstractNavigator(abc.ABC):
         Returns:
             The new length of the waypoint queue
         """
-        self._waypoints.pop(i)
-        return len(self._waypoints)
+        self.__last_waypoint = self.__waypoints.pop(i)
+        return len(self.__waypoints)
 
-    def view_waypoints(self):
+    def view_waypoints(self) -> List[Waypoint]:
         """
         Returns a copy of the current waypoint queue in order.
         To edit the waypoint queue, use the add_waypoint and del_waypoint methods
         """
-        return [waypoint for waypoint in self._waypoints]
+        return [waypoint for waypoint in self.__waypoints]
 
     def get_waypoint(self, i=0):
         """
         Returns the i-th waypoint, or the next waypoint if i is not specified
         """
-        if self._waypoints:
-            return self._waypoints[i]
+        if self.__waypoints:
+            return self.__waypoints[i]
         return None
-    
-    @property
-    def enabled(self):
-        return self._enabled
 
-    @enabled.setter
-    def enabled(self, new_val):
-        self._enabled = new_val
+    @property
+    def last_waypoint(self) -> Waypoint:
+        """
+        Returns the last reached waypoint, or the boat's initial location if not waypoint has been reached yet
+        """
+        return self.__last_waypoint
