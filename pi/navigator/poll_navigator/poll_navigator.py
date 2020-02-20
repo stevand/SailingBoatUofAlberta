e25456bcbaf757c25c98dabf0b5c061c8462b9e1
yash-bhandari <yashaswi@ualberta.ca>
2020-02-20 02:06:18 -0700

    rewrote poll_navigator to use interval_repeater

--- pi/navigator/poll_navigator/poll_navigator.py
+++ pi/navigator/poll_navigator/poll_navigator.py
@@ -1,5 +1,7 @@
 from threading import Thread
-from .. import AbstractNavigator
+from pi.navigator import AbstractNavigator
+from pi.helmsman import Helmsman
+from pi.boat_driver.abstract_boat_driver import AbstractBoatDriver
 from .voter import WaypointVoter, WindVoter, ChannelVoter, ManueverVoter
 from pi.navutils import dist
 from time import sleep
@@ -26,35 +28,44 @@ def navigate(interval, nav, driver, helmsman):
                 # print(goto, helmsman.desired_heading)
             else:
                 print('minimizing speed')
-                helmsman.maximize_speed = False 
+                helmsman.maximize_speed = False
         sleep(interval)
 
 
 class PollNavigator(AbstractNavigator):
-    def __init__(self, driver, helmsman, interval=0.3, num_headings=180, **kwargs):
+    def __init__(self, driver: AbstractBoatDriver, helmsman: Helmsman, num_headings=180, **kwargs):
         super().__init__(driver, helmsman, **kwargs)
+        self._curr_heading = -1  # will represent the current goal heading
+
         # constructs all necessary voters and their weights
-        self._prev = driver.get_position()
-        self.curr_heading = -1 #initialized to impossible value
         self._voters = [
             WaypointVoter(driver, self.get_waypoint),
             WindVoter(driver),
-            ChannelVoter(driver, lambda: (self._prev, self.get_waypoint())),
-            ManueverVoter(driver, lambda: self.curr_heading)
+            ChannelVoter(driver, lambda: (
+                self.last_waypoint, self.get_waypoint())),
+            ManueverVoter(driver, lambda: self._curr_heading)
         ]
         # constructs a dictionary that holds approval for each candidate heading
         self.headings = {2*i: 0 for i in range(num_headings)}
         helmsman.maximize_speed = True
-        thread = Thread(target=navigate, daemon=True,
-                        args=[interval, self, driver, helmsman])
-        thread.start()
+
+    def _navigate_to_curr_waypoint(self):
+        if not self.get_waypoint(): # stop moving if no more waypoints
+            print("minimizing speed")
+            self._helmsman.maximize_speed = False
+        else:
+            best_heading = self.poll()
+            print("Chosen heading:", best_heading)
+            print(*self.headings[best_heading])
+            self._helmsman.turn(best_heading)
+            self._curr_heading = best_heading
 
     def poll(self):
         """Polls all voters and returns the heading with the highest approval"""
         best_heading = -1
         for heading in self.headings.keys():
             self.headings[heading] = [voter.vote(heading)
-                                         for voter in self._voters]
+                                      for voter in self._voters]
             self.headings[heading].insert(0, sum(self.headings[heading]))
             if best_heading == -1 or self.headings[best_heading][0] < self.headings[heading][0]:
                 best_heading = heading
