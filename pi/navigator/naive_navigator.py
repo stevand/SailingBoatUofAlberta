6d892742a5b38052d39a204f03f008cf7bb9b0ea
yash-bhandari <yashaswi@ualberta.ca>
2020-02-20 20:53:19 -0700

    refactored naive_navigator to use interval_repeater

--- pi/navigator/naive_navigator.py
+++ pi/navigator/naive_navigator.py
@@ -1,45 +1,22 @@
 from . import AbstractNavigator
 from threading import Thread
 from time import sleep
-from pi.navutils import dist
+from pi.navutils import dist, dir_to_waypoint
+from pi.boat_driver.abstract_boat_driver import AbstractBoatDriver
+from pi.helmsman import Helmsman
 import math
 
-def angle_between(loc1, loc2):
-    """Finds the angle to turn the boat at loc1 to reach loc2"""
-    theta = math.atan2(loc2[1]-loc1[1], loc2[0]-loc1[0])
-    return math.degrees(theta) % 360
-
-
-def navigate(interval, get_waypoint, driver, helmsman, is_enabled, waypoint_dist, waypoint_reached):
-    while True:
-        if is_enabled():
-            next_waypoint = get_waypoint()
-            boat = driver.get_position()
-            while next_waypoint and dist(boat, next_waypoint) < waypoint_dist:
-                waypoint_reached()
-                next_waypoint = get_waypoint()
-
-            if next_waypoint:
-                helmsman.maximize_speed = True
-                goto = angle_between(boat, next_waypoint)
-                helmsman.turn(goto)
-                #print(goto, helmsman.desired_heading)
-            else:
-                helmsman.maximize_speed = False
-        sleep(interval)
-
-
 class NaiveNavigator(AbstractNavigator):
-    def __init__(self, driver, helmsman, interval=0.3, waypoint_dist=0.4, **kwargs):
-        super().__init__(driver, helmsman=helmsman, **kwargs)
+    def __init__(self, driver: AbstractBoatDriver, helmsman: Helmsman, **kwargs):
+        super().__init__(driver, helmsman,  **kwargs)
 
-        # interval is time between updating helmsman direction
-        self.interval = interval
-        # waypoint_dist is the distance at which the boat is considered to have reached the waypoint
-        self.waypoint_dist = waypoint_dist
+    def _navigate_to_curr_waypoint(self):
+        if not self.get_waypoint():
+            self._helmsman.maximize_speed = False
+        else:
+            self._helmsman.maximize_speed = True
+            heading = dir_to_waypoint(*self._driver.get_position(), *self.get_waypoint())
+            self._helmsman.turn(heading)
 
 
-        thread = Thread(target=navigate, daemon=True,
-            args=[interval, self.get_waypoint, driver, helmsman, lambda: self._enabled, waypoint_dist, self.del_waypoint])
-        thread.start()
 
