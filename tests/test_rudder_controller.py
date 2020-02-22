f5b58500d0a95e9eb1f8eb9d1af2d343328bfc7f
yash-bhandari <yashaswi@ualberta.ca>
2020-02-22 11:54:10 -0700

    updated ruddercontroller tests

--- tests/test_rudder_controller.py
+++ tests/test_rudder_controller.py
@@ -1,14 +1,11 @@
 import unittest
 import locator
 import pi.helmsman.rudder_controller as rc
+from pi.helmsman import RudderController
 from importlib import reload
 from time import sleep
 
 config_path = 'tests/configs/test_controllers.json'
-
-
-def is_enabled():
-    return True
 
 
 class TestRudderController(unittest.TestCase):
@@ -19,21 +16,15 @@ class TestRudderController(unittest.TestCase):
         self.driver = locator.get_driver()
 
     def test_tries_to_turn_left_100_20(self):
-        def get_desired_heading():
-            return 100
-
         self.driver.heading = 20
-        rc.start(self.driver, get_desired_heading, is_enabled, interval=.001)
+        self.rudder_controller = RudderController(self.driver, desired_heading=100, interval=0.001, enabled=True)
         sleep(0.003)
         self.assertLess(self.driver.get_rudder(), 0,
-                           'Boat did not try to turn left when the desired heading was 100 but the current heading was 20')
+                        'Boat did not try to turn left when the desired heading was 100 but the current heading was 20')
 
     def test_tries_to_turn_right_350_10(self):
-        def get_desired_heading():
-            return 350
-
         self.driver.heading = 10
-        rc.start(self.driver, get_desired_heading, is_enabled, interval=.001)
+        self.rudder_controller = RudderController(self.driver, desired_heading=350, interval=0.001, enabled=True)
         sleep(0.003)
         self.assertGreater(self.driver.get_rudder(), 0,
-                        'Boat did not try to turn right when the desired heading was 350 but the current heading was 10')
+                           'Boat did not try to turn right when the desired heading was 350 but the current heading was 10')
